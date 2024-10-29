a040c3b9cf4b42157e1ecab2ebc9c693313232c3
GKavvadias <il_gato_nero@hotmail.com>
2024-10-29 12:13:20 +0200

    Adding InfoBox component, Adding some css in index.css

--- src/components/GoalList.tsx
+++ src/components/GoalList.tsx
@@ -1,5 +1,7 @@
 import Goal from "./Goal";
 import { Goal as GoalProps } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type GoalListProps = {
   goals: GoalProps[];
@@ -7,15 +9,28 @@ type GoalListProps = {
 };
 
 export default function GoalList({ goals, onDelete }: GoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">You have set no goals yet!</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = <InfoBox mode="warning">You have set too many goals...</InfoBox>;
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <Goal id={goal.id} title={goal.title} onDelete={onDelete}>
-            <p>{goal.description}</p>
-          </Goal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <Goal id={goal.id} title={goal.title} onDelete={onDelete}>
+              <p>{goal.description}</p>
+            </Goal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
